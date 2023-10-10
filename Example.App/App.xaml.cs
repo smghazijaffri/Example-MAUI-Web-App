a0382ab7753c2bf038eedf451416c13bd778b05b
Ghazi <Ghazi@DESKTOP-GVLTI9D>
2023-10-10 19:42:39 +0500

    Add project files.

---
+++ Example.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Example.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Example.App/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Internals;
+using Microsoft.Maui.Controls.Platform;
+
+namespace Example.App
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
