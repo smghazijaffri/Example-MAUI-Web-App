9013cb28c92419cb7e2ddc1c905871578641540f
Ghazi Jaffri <ghazi.jaffri@hotmail.com>
2023-10-16 00:50:05 +0500

    Navbar

--- Example.WebComponents/wwwroot/exampleJsInterop.js
+++ Example.WebComponents/wwwroot/exampleJsInterop.js
@@ -2,5 +2,34 @@
 // functions, and may import other JavaScript modules if required.
 
 export function showPrompt(message) {
-  return prompt(message, 'Type anything here');
+    return prompt(message, 'Type anything here');
 }
+
+$(document).ready(function () {
+    (function ($) {
+        "use strict";
+
+        // Spinner
+        var spinner = function () {
+            setTimeout(function () {
+                if ($("#spinner").length > 0) {
+                    $("#spinner").removeClass("show");
+                }
+            }, 1);
+        };
+        spinner();
+
+        // Initiate the wowjs
+        new WOW().init();
+
+        // Sticky Navbar
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 40) {
+                $(".navbar").addClass("sticky-top");
+            } else {
+                $(".navbar").removeClass("sticky-top");
+            }
+        });
+    }
+});
+
