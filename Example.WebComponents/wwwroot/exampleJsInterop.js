be1452e7b95e0990ec2592b1c0e8eeec3e60dace
Ghazi Jaffri <ghazi.jaffri@hotmail.com>
2023-11-12 02:19:21 +0500

    1 file left

--- Example.WebComponents/wwwroot/exampleJsInterop.js
+++ Example.WebComponents/wwwroot/exampleJsInterop.js
@@ -5,6 +5,31 @@ export function showPrompt(message) {
     return prompt(message, 'Type anything here');
 }
 
+$(document).ready(function () {
+    (function ($) {
+        "use strict";
+
+        // Spinner
+        var spinner = function () {
+            setTimeout(function () {
+                if ($("#spinner").length > 0) {
+                    $("#spinner").removeClass("show");
+                }
+            }, 1);
+        };
+        spinner();
+
+        // Sticky Navbar
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 40) {
+                $(".navbar").addClass("sticky-top");
+            } else {
+                $(".navbar").removeClass("sticky-top");
+            }
+        });
+    }
+});
+
 // wwwroot/js/custom.js
 
 function openLoginModal() {
